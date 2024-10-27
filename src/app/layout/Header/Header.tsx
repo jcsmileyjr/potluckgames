89d14c6951dc119eba97699777ba1d4e43ecc775
JC <jcsmileyjr@gmail.com>
2024-10-27 11:22:01 -0500

    Refactor Header: Ensure the registration link stands out

--- src/app/layout/Header/Header.tsx
+++ src/app/layout/Header/Header.tsx
@@ -3,12 +3,14 @@ export default function Header() {
     return (
         <section className="relative" >
           <div className="h-80 w-full absolute inset-0 opacity-20 aspect-square bg-no-repeat  bg-cover bg-center bg-[url('/primaryimage.jpeg')] "></div>
-          <div className="h-80 relative z-10">
+          <div className="h-80 relative z-10 flex flex-col justify-around">
             <h1 className="text-accentRed font-bold text-center text-4xl py-2">Potluck & Games</h1>
-            <div className="px-12 flex flex-col gap-3 text-black">
+            <div className="px-12 flex flex-col gap-1 text-black">
               <p className="text-xl">End of the year Celebration Meal with fellow Tech Professionals</p>
               <p className="text-bass">FedEx Institute of Technology @ The University of MemphisNov. 22nd @ 6pm</p>
-              <p className="text-base">Don’t  forget to sign up on Meetup.com at thiswillbereplacewithsomething. </p>
+            </div>
+            <div className="px-12 mt-4 flex flex-col gap-3 text-black">
+                <button type="button" className="bg-accentRed text-white py-2 px-4 rounded-md">Meetup.com Registration</button>
             </div>
           </div>
         </section>
