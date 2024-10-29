a1c6f35b68e12caec32f330149d8ebf51d6fdca5
JC <jcsmileyjr@gmail.com>
2024-10-29 15:24:15 -0500

    Style: View Dish - Fix whitespace between content areas

--- src/app/components/FoodToggle/FoodToggle.tsx
+++ src/app/components/FoodToggle/FoodToggle.tsx
@@ -5,9 +5,11 @@ import Cavet from '../../assets/images/caret.png';
 export default function FoodToggle({details}: Readonly<{details: UserSummary}>) {
     return (
         <details>
-            <summary className="flex flex-row justify-between items-center text-primaryBrown">
-                <p>{details.userName}</p>
-                <p>{details.dishType}: {details.dishName}</p>
+            <summary className="flex flex-row gap-8 justify-between items-center text-primaryBrown">
+                <div className="flex-3 flex flex-row gap-8 sm:gap-16">
+                    <p>{details.userName}</p>
+                    <p>{details.dishType}: {details.dishName}</p>
+                </div>
                 <Image className="h-4" src={Cavet} width={20} height={5} alt="rotating cavet"/>
             </summary>
             <p className="text-blackaccent1 pl-4">{details.dishdescription}</p>
