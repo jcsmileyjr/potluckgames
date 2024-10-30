52298be09a767e2d811978f65c7962619d53c3a2
JC <jcsmileyjr@gmail.com>
2024-10-30 14:28:25 -0500

    Style: FoodToggle - Fix details toggle of icon

--- src/app/components/FoodToggle/FoodToggle.tsx
+++ src/app/components/FoodToggle/FoodToggle.tsx
@@ -7,13 +7,13 @@ import Cavet from '../../assets/images/caret.png';
 export default function FoodToggle({details}: Readonly<{details: UserSummary}>) {
     const [open, setOpen] = useState(false);
     return (
-        <details open={open} onClick={() => setOpen(!open)}>
+        <details open={open} onToggle={() => setOpen(!open)}>
             <summary className="flex flex-row gap-8 justify-between items-center text-primaryBrown ">
                 <div className="flex-3 flex flex-row gap-8 sm:gap-16">
                     <p>{details.userName}</p>
                     <p>{details.dishType}: {details.dishName}</p>
                 </div>
-                <Image className={`h-4 ${!open ? 'rotate-0 transition duration-800' : 'rotate-180 transition duration-800'}`}  src={Cavet} width={20} height={5} alt="rotating cavet"/>
+                <Image className={`h-4 ${open ? 'rotate-0 transition duration-800' : 'rotate-180 transition duration-800'}`}  src={Cavet} width={20} height={5} alt="rotating cavet"/>
             </summary>
             <p className="text-blackaccent1 pl-4">{details.dishdescription}</p>
         </details>
