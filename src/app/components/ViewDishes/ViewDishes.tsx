60ecef4e861a2800dba53aef3f05edf2d3741980
JC <jcsmileyjr@gmail.com>
2024-11-01 17:03:26 -0500

    build-issue: View Dish - Un-used data broke build

--- src/app/components/ViewDishes/ViewDishes.tsx
+++ src/app/components/ViewDishes/ViewDishes.tsx
@@ -3,24 +3,6 @@ import { useState, useEffect } from "react";
 import { getFood } from "@/app/actions/getFood";
 import FoodToggle from "../FoodToggle/FoodToggle";
 import { UserSummary } from "../Types";
-const UserSummaries: UserSummary[] = [
-    {
-        id: 1,
-        user_name: "John Doe",
-        user_email: "8VHs8@example.com",
-        dish_type: "Meat",
-        dish_name: "Turkey N Dressing",
-        dish_description: "Slow cooked rotisserie  turkey with buttery, savory, melt-in-your-mouth stuffing"
-    },
-    {
-        id: 2,
-        user_name: "Jane Doe",
-        user_email: "8VHs8@example.com",
-        dish_type: "Bread",
-        dish_name: "Butter rolls",
-        dish_description: "This is a description of the dish"
-    }
-]
 
 export default function ViewDishes() {
     const [UserSummaries, setUserSummaries] = useState<UserSummary[]>([]);
