1c03bbc1996fd680ac6c1b8e9b3c60449ff9499a
JC <jcsmileyjr@gmail.com>
2024-11-02 10:39:45 -0500

    Refactor app to clear the inputs fields for the add dish section

--- src/app/components/UserInput/UserInput.tsx
+++ src/app/components/UserInput/UserInput.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-export default function UserInput({inputName, content, description, getValue}: Readonly<{inputName: string, content: string, description: string | boolean, getValue: (e: React.ChangeEvent<HTMLInputElement>) => void}>) {
+export default function UserInput({value, inputName, content, description, getValue}: Readonly<{value: string, inputName: string, content: string, description: string | boolean, getValue: (e: React.ChangeEvent<HTMLInputElement>) => void}>) {
     return (
         <div className="mb-4">
             <label htmlFor={inputName} className="block text-base font-bold text-primaryBrown"><span className="text-red align-text-top">*</span>{content}</label>
-            <input name={inputName} onChange={(e) => getValue(e)} id={inputName} type={inputName === 'user_email' ? 'email' : 'text'} className="w-full my-1 p-2 border border-black rounded-md text-black" />
+            <input value={value} name={inputName} onChange={(e) => getValue(e)} id={inputName} type={inputName === 'user_email' ? 'email' : 'text'} className="w-full my-1 p-2 border border-black rounded-md text-black" />
             {description !== true &&
                 <p className="text-blackaccent2 text-sm">{description} </p>
             }
