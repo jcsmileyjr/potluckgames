4c6e4cb75e105d840ba0efd81586b0c754fbef17
JC <jcsmileyjr@gmail.com>
2024-11-06 14:48:52 -0600

    WIP Update Attendee page - Initial setup

---
+++ src/app/UpdateAttendee/page.tsx
@@ -0,0 +1,12 @@
+import UpdateDish from "../components/UpdateDish/UpdateDish";
+
+export default async function UpdateAttendee() {
+    return (
+        <main>
+            <header>
+                <h1 className="text-accentRed font-bold text-center text-4xl py-2">Potluck & Games</h1>
+            </header>
+            <UpdateDish />
+        </main>
+    )
+}
