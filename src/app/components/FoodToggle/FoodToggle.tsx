cce6b60683a32217a3d2bcc664fea87f78c87742
JC <jcsmileyjr@gmail.com>
2024-11-07 12:20:32 -0600

    Style: style the pointer

--- src/app/components/FoodToggle/FoodToggle.tsx
+++ src/app/components/FoodToggle/FoodToggle.tsx
@@ -8,7 +8,7 @@ export default function FoodToggle({details}: Readonly<{details: UserSummary}>) 
     const [open, setOpen] = useState(false);
     return (
         <details open={open} onToggle={() => setOpen(!open)}>
-            <summary className="flex flex-row items-center text-primaryBrown ">
+            <summary className="flex flex-row items-center text-primaryBrown cursor-pointer">
                 <p className="w-1/3">{details.user_name}</p>
                 <p className="flex-1">{details.dish_type}: {details.dish_name}</p>
                 <Image className={`h-4  ${open ? 'rotate-0 transition duration-800' : 'rotate-180 transition duration-800'}`}  src={Cavet} width={20} height={5} alt="rotating cavet"/>
